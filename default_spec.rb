f35bbcf49062d096718f2584115aece353f999bc
sgabriel62 <45497213+sgabriel62@users.noreply.github.com>
2018-11-30 15:12:15 -0500

    Update default_spec.rb

--- default_spec.rb
+++ default_spec.rb
@@ -1,7 +1,7 @@
-describe package('Alteryx 11.5 x64') do
+describe package('Alteryx 2018.3 x64') do
   it { should_not be_installed }
 end
 
-describe package('Alteryx Predictive Tools with R 3.3.2') do
+describe package('Alteryx Predictive Tools with R 2018.3.4.51585') do
   it { should_not be_installed }
 end
