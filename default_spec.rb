d3bcfc2be0a3b918c01fb910f208a85213553057
sgabriel62 <45497213+sgabriel62@users.noreply.github.com>
2018-11-30 15:30:18 -0500

    Delete default_spec.rb

--- default_spec.rb
+++
@@ -1,7 +0,0 @@
-describe package('Alteryx 2018.3 x64') do
-  it { should_not be_installed }
-end
-
-describe package('Alteryx Predictive Tools with R 2018.3.4.51585') do
-  it { should_not be_installed }
-end
