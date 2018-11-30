191f9395ff09b45a2cbe0ed9a75a970e04e971aa
sgabriel62 <45497213+sgabriel62@users.noreply.github.com>
2018-11-30 15:52:43 -0500

    Create default.rb

---
+++ attributes/default.rb
@@ -0,0 +1,21 @@
+default['alteryx'] = {}
+default['alteryx']['installer_timeout'] = 3600
+default['alteryx']['r_installer_timeout'] =
+  default['alteryx']['installer_timeout']
+default['alteryx']['license'] = {}
+default['alteryx']['force_secrets_update'] = nil
+default['alteryx']['r_source'] = nil
+default['alteryx']['r_version'] = nil
+default['alteryx']['restart_on_change'] = false
+default['alteryx']['source'] = nil
+default['alteryx']['version'] = '2018.3.5.52487'
+default['alteryx']['runtimesettings']['engine']['num_threads'] =
+  node['cpu']['total'] + 1
+default['alteryx']['runtimesettings']['engine']['sort_join_memory'] =
+  (node['kernel']['os_info']['total_visible_memory_size'].to_i * 0.8 /
+  1024 / (node['cpu']['total'] + 1)).to_i
+default['alteryx']['rts_defaults_path'] =
+  'C:\\Program Files\\Alteryx\\bin\\RuntimeData\\RuntimeSettings.xml'
+default['alteryx']['rts_overrides_path'] =
+  'C:\\ProgramData\\Alteryx\\RuntimeSettings.xml'
+© 2018 GitHub, Inc.
